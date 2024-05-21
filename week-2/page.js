ff49c6afd02543e2ab98b600b5922ee49cd934d7
D-Sluchinski <Derek.Sluchinski@edu.sait.ca>
2024-05-21 14:05:04 -0600

    cprg306: move student info to page
    remove student info and place it inside page

--- week-2/page.js
+++ week-2/page.js
@@ -1,7 +1,8 @@
 export default function Page() {
   return (
     <main>
-      <h1>Shopping List</h1>
+      <h1>Derek</h1>
+      <h3>https://github.com/D-Sluchinski/cprg306-assignments.git</h3>
     </main>
   );
 }
--- week-2/student-info.js
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-    return (
-      <main>
-        <h1>Derek Sluchinski</h1>
-        <h3>https://github.com/D-Sluchinski/cprg306-assignments.git</h3>
-        <h4></h4>
-      </main>
-    );
-  }
