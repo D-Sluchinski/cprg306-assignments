f5a8ede683286704963d657e7f4f9dd6bfe90b62
D-Sluchinski <Derek.Sluchinski@edu.sait.ca>
2024-06-10 09:40:56 -0600

    CPRG306: week 4 update
    finalize week 4 submission.

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -6,25 +6,20 @@ export default function NewItem (){
     const [quantity, setQuantity] = useState(1);
     const [category, setCategory] = useState("produce");
 
-    // name field
+    // NAME FIELD
 	const handleTextChange = (event) =>
-        {
-        
+        {  
         const text = event.target.value;
         setName(text);
-        
-        }
-
+        };
 
 
     const handleQuantityChange = (event) =>
         {
-        
         const text = event.target.value;
         setQuantity(text);
-        }
-
-
+        };
+    
 
     const handleCategoryChange = (event) =>
         {
@@ -33,9 +28,9 @@ export default function NewItem (){
         setCategory(text);
         };
         
-
+        //JUST FOR DROP DOWN
         const category1 = {
-            categoryName: ("produce"),
+            categoryName: "produce",
         };
         const category2 = {
             categoryName: "dairy",
@@ -75,28 +70,53 @@ export default function NewItem (){
             event.preventDefault();
 
             let addNewItem = {name: name, quantity: quantity, category: category};
-            console.log(addNewItem);
-            alert(`name: ${addNewItem.name} quantity: ${addNewItem.quantity}, category: ${addNewItem.category}`)
+            
+            //REQUIREMENT HANDLER
+            if(addNewItem.name == "" || addNewItem.quantity > 100 || addNewItem.quantity < 1){
+                
+                if(addNewItem.name == "")
+                    {
+                        alert("Name required");
+                    };
+                
+                if (addNewItem.quantity > 100 || addNewItem.quantity < 1){
+                    alert("quantity required to be between 1 & 100");
+                };
+                    
+            }
+
+            else
+                alert(`name: ${addNewItem.name} quantity: ${addNewItem.quantity}, category: ${addNewItem.category}`);
+
+            //LOG ITEM INTO CONSOLE
+            console.log(addNewItem.name + addNewItem.quantity + addNewItem.category );
+
+            //RESETS
+            setName("");
+            setCategory("Produce");
+            setQuantity(1);
+
+
         };
     
 
         return(
             <form class="p-3 m-10 bg-slate-900 max-w-sm w-full rounded-lg" action="">  
-                
+                {/* ITEM NAME */}
                 <div class="pt-8 pb-8 ">
-                    {/* idk why the required feilds arent working */}
-                    <input type="text" required="" onChange={handleTextChange} 
-                        Class="border-2 bg-black p-4 rounded-lg w-full" placeholder="item" value=""/>
+                    <input type="text" required onChange={handleTextChange} 
+                        Class="border-2 bg-black p-4 rounded-lg w-full" placeholder="item" value={name}/>
                 </div>
 
-                
+
+                {/* QUANTITY                 */}
                 <div class="flex justify-between-8">
                     <input type="number" required value={quantity} onChange={handleQuantityChange} 
                     Class="border-2 bg-black p-4 rounded-lg mr-10 min-w-5" min="1" max="99"/>
-
+                    {/* DROP DOWN LIST */}
                     <select value={category} onChange={handleCategoryChange} Class="border-2 bg-black p-4 rounded-lg w-full">
                             <option value="" disabled >Category</option>
-                            <option value="produce" selected="">{category1.categoryName.charAt(0).toUpperCase() + category1.categoryName.slice(1)}</option>
+                            <option value="produce" selected>{category1.categoryName.charAt(0).toUpperCase() + category1.categoryName.slice(1)}</option>
                             <option value="dairy" >{category2.categoryName.charAt(0).toUpperCase() + category2.categoryName.slice(1)}</option>
                             <option value="bakery">{category3.categoryName.charAt(0).toUpperCase() + category3.categoryName.slice(1)}</option>
                             <option value="meat">{category4.categoryName.charAt(0).toUpperCase() + category4.categoryName.slice(1)}</option>
