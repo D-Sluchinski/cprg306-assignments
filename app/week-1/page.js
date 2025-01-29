96a0fafc18ba91d65dff433ba934febcefa1e02c
D-Sluchinski <Derek.Sluchinski@edu.sait.ca>
2025-01-28 17:56:48 -0700

    week-1
    add vercel link

--- app/week-1/page.js
+++ app/week-1/page.js
@@ -6,8 +6,13 @@ export default function Page() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="p-5"><Link href={'/'} className="hover:underline">Back</Link></div>
         <div>
-            <h1>Derek Sluchinski</h1>
+            <h1 className="text-2xl">Derek Sluchinski</h1>
+            <div>
             <Link href={'https://github.com/D-Sluchinski/cprg306-assignments'} className="hover:underline">Github Link</Link>
+            </div>
+            <div>
+            <Link href={'https://cprg306-assignments-sooty.vercel.app/'} className="hover:underline">Vercel Link</Link>
+            </div>
         </div>
     </div>
   );
